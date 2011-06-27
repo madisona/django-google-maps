6f7e8a7e02d1d25264d049131f8dd81ede8537fb
Aaron Madison <aaron.madison@theimtgroup.com>
2011-06-27 13:54:57 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+
+import os
+from setuptools import setup
+
+from django_google_maps import VERSION
+
+REQUIREMENTS = [
+    'django',
+    'mock',
+]
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+LONG_DESCRIPTION = open(README, 'r').read()
+CLASSIFIERS = [
+    "Development Status :: 3 - Alpha",
+    "Environment :: Web Environment",
+    "Framework :: Django",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries :: Application Frameworks",
+]
+
+setup(
+    name="django-google-maps",
+    version=VERSION,
+    author="Aaron Madison",
+    author_email="aaron.l.madison@gmail.com",
+    description="Plugs google maps V3 api into Django admin.",
+    long_description=LONG_DESCRIPTION,
+    url="https://github.com/madisona/django-google-maps",
+    packages=("django_google_maps",),
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    zip_safe=False,
+)
