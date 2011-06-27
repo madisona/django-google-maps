9a8debecddec7e606391d0bd85df09bf6f7ba548
Aaron Madison <aaron.madison@theimtgroup.com>
2011-06-27 13:45:36 -0500

    changed GeoPt field to allow empty instantiation of class
    also, fixed the GeoPt field to use __metaclass__ = models.SubfieldBase so we are certain the to_python method is always called.

--- django_google_maps/fields.py
+++ django_google_maps/fields.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.core import exceptions
 
@@ -12,25 +11,37 @@ def typename(obj):
         return type(obj).__name__
 
 class GeoPt(object):
-    "A geographical point."
+    """A geographical point."""
 
     lat = None
     lon = None
 
     def __init__(self, lat, lon=None):
+        """
+        If the model field has 'blank=True' or 'null=True' then
+        we can't always expect the GeoPt to be instantiated with
+        a valid value. In this case we'll let GeoPt be instantiated
+        as an empty item, and the string representation should be
+        an empty string instead of 'lat,lon'.
+        """
+        if not lat:
+            return
+
         if lon is None:
             lat, lon = self._split_geo_point(lat)
         self.lat = self._validate_geo_range(lat, 90)
         self.lon = self._validate_geo_range(lon, 180)
 
     def __unicode__(self):
-        return "%s,%s" % (self.lat, self.lon)
+        if self.lat is not None and self.lon is not None:
+            return "%s,%s" % (self.lat, self.lon)
+        return ''
 
     def __len__(self):
         return len(self.__unicode__())
 
     def _split_geo_point(self, geo_point):
-        "splits the geo point into lat and lon"
+        """splits the geo point into lat and lon"""
         try:
             return geo_point.split(',')
         except (AttributeError, ValueError):
@@ -55,7 +66,8 @@ class AddressField(models.CharField):
     pass
 
 class GeoLocationField(models.CharField):
-    """A geographical point, specified by floating-point latitude and longitude
+    """
+    A geographical point, specified by floating-point latitude and longitude
     coordinates. Often used to integrate with mapping sites like Google Maps.
     May also be used as ICBM coordinates.
 
@@ -67,6 +79,7 @@ class GeoLocationField(models.CharField):
     ranges [-90, 90] and [-180, 180], respectively.
     """
     description = "A geographical point, specified by floating-point latitude and longitude coordinates."
+    __metaclass__ = models.SubfieldBase
 
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 100
@@ -78,8 +91,8 @@ class GeoLocationField(models.CharField):
         return GeoPt(value)
 
     def get_prep_value(self, value):
-        "prepare the value for database query"
-        return "%s,%s" % (value.lat, value.lon)
+        """prepare the value for database query"""
+        return str(value)
 
     def get_prep_lookup(self, lookup_type, value):
         # We only handle 'exact' and 'in'. All others are errors.
