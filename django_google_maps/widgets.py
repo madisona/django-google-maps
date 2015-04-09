df931b1245a67e401118b1d853d7651285ef1626
Aaron Madison <aaron.l.madison@gmail.com>
2015-04-09 06:22:12 -0500

    Dependency: Updated for django 1.7 & 1.8 Compatibility

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -3,16 +3,16 @@ from django.conf import settings
 from django.forms import widgets
 from django.utils.encoding import force_unicode
 from django.utils.safestring import mark_safe
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 
 
 class GoogleMapsAddressWidget(widgets.TextInput):
     "a widget that will place a google map right after the #id_address field"
 
     class Media:
-        css = {'all': (settings.STATIC_URL + 'django_google_maps/css/google-maps-admin.css')}
+        css = {'all': (settings.STATIC_URL + 'django_google_maps/css/google-maps-admin.css',)}
         js = (
-            'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
             'https://maps.google.com/maps/api/js?sensor=false',
             settings.STATIC_URL + 'django_google_maps/js/google-maps-admin.js',
         )
