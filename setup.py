1522367b6b31a75fe189295b43eb9c304550977b
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2015-05-06 19:15:41 +0300

    move requirements out of directory, update setup file

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
-
-import os
+#!/usr/bin/env python
 from setuptools import setup
+from django_google_maps import __version__
 
-README = os.path.join(os.path.dirname(__file__), 'README.md')
-LONG_DESCRIPTION = open(README, 'r').read()
-CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
-    "Environment :: Web Environment",
-    "Framework :: Django",
-    "Intended Audience :: Developers",
-    "License :: OSI Approved :: BSD License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python",
-    "Topic :: Software Development",
-    "Topic :: Software Development :: Libraries :: Application Frameworks",
-]
 
 setup(
-    name="django-google-maps",
-    version='0.3.0',
-    author="Aaron Madison",
-    author_email="aaron.l.madison@gmail.com",
-    description="Plugs google maps V3 api into Django admin.",
-    long_description=LONG_DESCRIPTION,
-    url="https://github.com/madisona/django-google-maps",
-    packages=("django_google_maps",),
+    name='django-google-maps',
+    version=__version__,
+    author='Aaron Madison',
+    author_email='aaron.l.madison@gmail.com',
+    description='Plugs google maps V3 api into Django admin.',
+    long_description=open('README.md', 'r').read(),
+    url='https://github.com/madisona/django-google-maps',
+    packages=['django_google_maps'],
     include_package_data=True,
-    install_requires=file('requirements/requirements.txt').read().split('\n'),
-    classifiers=CLASSIFIERS,
+    install_requires=open('requirements.txt').read().split('\n'),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
     zip_safe=False,
+    platforms='any',
 )
