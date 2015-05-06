1bf9d4a56ad06f7521db619e713b461ef2774a89
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2015-05-06 20:49:56 +0300

    add custom test command for setuptools

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import ast
 import re
-from setuptools import setup
+from setuptools import Command, setup
 
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
@@ -9,6 +9,23 @@ _version_re = re.compile(r'__version__\s+=\s+(.*)')
 with open('django_google_maps/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
+
+
+class Test(Command):
+    description = 'Custom test runner'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from run_tests import setup
+        from django.core.management import call_command
+        setup()
+        call_command('test')
 
 
 setup(
@@ -22,6 +39,7 @@ setup(
     packages=['django_google_maps'],
     include_package_data=True,
     install_requires=open('requirements.txt').read().split('\n'),
+    tests_require=open('requirements-dev.txt').read().split('\n')[1:],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Web Environment',
@@ -36,4 +54,5 @@ setup(
     ],
     zip_safe=False,
     platforms='any',
+    cmdclass={'test': Test},
 )
