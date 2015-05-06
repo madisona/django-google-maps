bc01c51e45d41024dda45f9a4911dbd5d1ac6290
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2015-05-06 18:42:20 +0300

    remove sample project files

--- sample/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-from django_google_maps.fields import AddressField, GeoLocationField
-
-
-class SampleModel(models.Model):
-    address = AddressField(max_length=100)
-    geolocation = GeoLocationField(blank=True)
