ed5ec601310e0c63f30dc07a9cf319d0a9a6e6af
Aaron Madison <aaron.l.madison@gmail.com>
2016-07-05 13:43:40 -0500

    Enhancement: added python3 compatibility

--- django_google_maps/tests/test_typename.py
+++ django_google_maps/tests/test_typename.py
@@ -1,13 +1,19 @@
 from django import test
+from django.utils import six
 from django_google_maps.fields import typename
 
 
 class TypeNameTests(test.TestCase):
 
     def test_simple_type_returns_type_name_as_string(self):
-        self.assertEqual("str", typename("x"))
+        self.assertEqual('str', typename("x"))
 
     def test_class_object(self):
         class X:
             pass
-        self.assertEqual("classobj", typename(X))
+
+        if six.PY2:
+            expected_type = 'classobj'
+        else:
+            expected_type = 'type'
+        self.assertEqual(expected_type, typename(X))
