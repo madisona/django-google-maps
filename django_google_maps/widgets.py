1ff2acb5bef109640cd82c982a52150bd5f3689e
Aaron Madison <aaron.l.madison@gmail.com>
2016-08-12 11:58:22 -0500

    Dependency: Bumped to latest jquery v3.1.0

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -15,7 +15,7 @@ class GoogleMapsAddressWidget(widgets.TextInput):
     class Media:
         css = {'all': (settings.STATIC_URL + 'django_google_maps/css/google-maps-admin.css',)}
         js = (
-            'https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+            'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js',
             'https://maps.google.com/maps/api/js?key={}'.format(settings.GOOGLE_MAPS_API_KEY),
             settings.STATIC_URL + 'django_google_maps/js/google-maps-admin.js',
         )
