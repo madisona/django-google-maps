f8a6a02fe51e9a89ae46cb42f916d6e6f68e13a4
Aaron Madison <aaron.l.madison@gmail.com>
2016-11-07 20:44:31 -0600

    Peripheral: Updated sample app to work with django 1.8+

--- sample/models.py
+++ sample/models.py
@@ -6,3 +6,6 @@ from django_google_maps.fields import AddressField, GeoLocationField
 class SampleModel(models.Model):
     address = AddressField(max_length=100)
     geolocation = GeoLocationField(blank=True)
+
+    def __str__(self):
+        return self.address
--- settings.py
+++ settings.py
@@ -1,7 +1,6 @@
 # Django settings for google_maps project.
 
 DEBUG = True
-TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -82,15 +81,22 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+            ]
+        },
+    },
+]
+
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '%hi+eb@u)t)o_qk^#y&eje%*65ghba=1xulgk$_zfx5#&b3$g4'
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -102,11 +108,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
