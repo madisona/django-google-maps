ad52a32d89a6aae0916bf5c1c74fa0acb573a605
Aaron Madison <aaron.l.madison@gmail.com>
2018-03-04 06:34:23 -0600

    Enhancement: Add sample for non-admin use

--- sample/views.py
+++ sample/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import FormView
+
+from sample.forms import SampleForm
+
+
+class SampleFormView(FormView):
+    form_class = SampleForm
+    template_name = "sample/index.html"
