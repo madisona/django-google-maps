e4abfe288b8936df71129d89abc8b3c4e2fe5635
Synkied <synkx@hotmail.fr>
2018-05-31 15:10:28 +0200

    added default language depending on os language

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -1,5 +1,17 @@
+import locale
+
 from django.conf import settings
 from django.forms import widgets
+
+if hasattr(settings, 'GOOGLE_MAPS_DEFAULT_LANGUAGE'):
+    GOOGLE_MAPS_DEFAULT_LANGUAGE = settings.GOOGLE_MAPS_DEFAULT_LANGUAGE
+
+else:
+    if locale.getdefaultlocale():
+        GOOGLE_MAPS_DEFAULT_LANGUAGE = locale.getdefaultlocale()[0]
+
+    else:
+        GOOGLE_MAPS_DEFAULT_LANGUAGE = 'en'
 
 
 class GoogleMapsAddressWidget(widgets.TextInput):
@@ -14,6 +26,6 @@ class GoogleMapsAddressWidget(widgets.TextInput):
         js = (
             'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js',
             'https://maps.google.com/maps/api/js?key={}&libraries=places&language={}'.format(
-                settings.GOOGLE_MAPS_API_KEY, settings.GOOGLE_MAPS_DEFAULT_LANGUAGE),
+                settings.GOOGLE_MAPS_API_KEY, GOOGLE_MAPS_DEFAULT_LANGUAGE),
             settings.STATIC_URL + 'django_google_maps/js/google-maps-admin.js',
         )
