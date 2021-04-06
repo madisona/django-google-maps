3d94a89ef2e7a02cb496fc41ffe6f9bc6eb6cf2b
Aaron <aaron.l.madison@gmail.com>
2021-04-06 08:25:27 -0500

    Peripheral: Prepare release (#82)

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ CLASSIFIERS = [
 
 setup(
     name="django-google-maps",
-    version='0.12.3',
+    version='0.12.4',
     author="Aaron Madison",
     author_email="aaron.l.madison@gmail.com",
     description="Plugs google maps V3 api into Django admin.",
