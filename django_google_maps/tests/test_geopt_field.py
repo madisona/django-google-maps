2a290c9c217ccf4a3775b23874defa7f30b5e647
Aaron Madison <aaron.l.madison@gmail.com>
2021-11-19 11:36:44 -0600

    Refactor: update force_text to force_str
    [https://github.com/madisona/django-google-maps/issues/83]

--- django_google_maps/tests/test_geopt_field.py
+++ django_google_maps/tests/test_geopt_field.py
@@ -1,6 +1,6 @@
 from django import test
 from django.core import exceptions
-from django.utils.encoding import force_text
+from django.utils.encoding import force_str
 
 from django_google_maps import fields
 
@@ -14,7 +14,7 @@ class GeoPtFieldTests(test.TestCase):
     def test_uses_lat_comma_lon_as_unicode_representation(self):
         lat_lon_string = "15.001,32.001"
         geo_pt = fields.GeoPt(lat_lon_string)
-        self.assertEqual(lat_lon_string, force_text(geo_pt))
+        self.assertEqual(lat_lon_string, force_str(geo_pt))
 
     def test_two_GeoPts_with_same_lat_lon_should_be_equal(self):
         geo_pt_1 = fields.GeoPt("15.001,32.001")
@@ -43,7 +43,7 @@ class GeoPtFieldTests(test.TestCase):
 
     def test_uses_empty_string_as_unicode_representation_for_empty_GeoPt(self):
         geo_pt = fields.GeoPt('')
-        self.assertEqual('', force_text(geo_pt))
+        self.assertEqual('', force_str(geo_pt))
 
     def test_splits_geo_point_on_comma(self):
         pt = fields.GeoPt("15.001,32.001")
