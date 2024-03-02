e1d6f0fdd5d688d0dc872777950b2a8fcbd8f143
CY Kalu <cy23@tutanota.com>
2024-03-02 22:02:49 +0000

    Add admin sample for inline location form

--- urls.py
+++ urls.py
@@ -1,16 +1,17 @@
-
 import django
 from django.contrib import admin
+
 admin.autodiscover()
 
 
-if django.get_version() >= '2.0.0':
+if django.get_version() >= "2.0.0":
     from django.urls import re_path as url
 else:
     from django.conf.urls import url
-from sample.views import SampleFormView
+
+from sample.views import LocationFormView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', SampleFormView.as_view()),
+    url(r"^admin/", admin.site.urls),
+    url(r"^$", LocationFormView.as_view()),
 ]
