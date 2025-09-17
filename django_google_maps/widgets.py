186bd2defc5d9d1a39bd9a29c222b51252c180f9
aaronmadison <aaron.l.madison@gmail.com>
2025-09-16 20:48:11 -0500

    Enhancement: use django 5.2 script object when available

--- django_google_maps/widgets.py
+++ django_google_maps/widgets.py
@@ -1,5 +1,12 @@
+import django
 from django.conf import settings
 from django.forms import widgets
+
+# Check if we're on Django 5.2+
+USE_SCRIPT_OBJECT = django.VERSION >= (5, 2)
+
+if USE_SCRIPT_OBJECT:
+    from django.forms.widgets import Script
 
 
 class GoogleMapsAddressWidget(widgets.TextInput):
@@ -9,7 +16,18 @@ class GoogleMapsAddressWidget(widgets.TextInput):
 
     class Media:
         css = {"all": ("django_google_maps/css/google-maps-admin.css",)}
-        js = (
-            f"https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&loading=async&callback=initGoogleMap",  # noqa: E501
-            "django_google_maps/js/google-maps-admin.js",
-        )
+
+        if USE_SCRIPT_OBJECT:
+            js = (
+                Script(
+                    f'https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&loading=async&callback=initGoogleMap',  # noqa: E501
+                    **{
+                        "async": True,
+                    }),
+                'django_google_maps/js/google-maps-admin.js',
+            )
+        else:
+            js = (
+                f"https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&loading=async&callback=initGoogleMap",  # noqa: E501
+                'django_google_maps/js/google-maps-admin.js',
+            )
