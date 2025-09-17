34250bb67431d4d2d5bb561117431a5618858ca4
aaronmadison <aaron.l.madison@gmail.com>
2025-09-16 20:19:26 -0500

    Enhancement: Move inline styling to css

--- django_google_maps/tests/test_widget.py
+++ django_google_maps/tests/test_widget.py
@@ -7,7 +7,7 @@ class WidgetTests(test.TestCase):
     def test_render_returns_xxxxxxx(self):
         widget = GoogleMapsAddressWidget()
         results = widget.render("name", "value", attrs={"a1": 1, "a2": 2})
-        expected = '<div style="display: flex; flex-direction: column;">'
+        expected = '<div class="map_widget_wrapper">'
         expected += '<input a1="1" a2="2" name="name" type="text" value="value" />'
         expected += '<div class="map_canvas_wrapper">'
         expected += '<div id="map_canvas"></div></div>'
@@ -17,7 +17,7 @@ class WidgetTests(test.TestCase):
     def test_render_returns_blank_for_value_when_none(self):
         widget = GoogleMapsAddressWidget()
         results = widget.render("name", None, attrs={"a1": 1, "a2": 2})
-        expected = '<div style="display: flex; flex-direction: column;">'
+        expected = '<div class="map_widget_wrapper">'
         expected += '<input a1="1" a2="2" name="name" type="text" />'
         expected += '<div class="map_canvas_wrapper">'
         expected += '<div id="map_canvas"></div></div>'
@@ -27,6 +27,7 @@ class WidgetTests(test.TestCase):
     def test_maps_js_uses_api_key(self):
         widget = GoogleMapsAddressWidget()
         google_maps_js = (
-            f"https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&loading=async&callback=initGoogleMap"  # noqa: E501
+            f"https://maps.googleapis.com/maps/api/js?key={settings.GOOGLE_MAPS_API_KEY}&loading=async&callback=initGoogleMap"
+        # noqa: E501
         )
         self.assertEqual(google_maps_js, widget.Media().js[0])
